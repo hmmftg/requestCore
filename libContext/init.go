16a8992ec3e751205745a55b72192af490000e00
hmmftg <hmmftg@gmail.com>
2023-08-30 14:52:33 +0330

    fix: handler initContext cat type

--- libContext/init.go
+++ libContext/init.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/hmmftg/requestCore/libFiber"
 	"github.com/hmmftg/requestCore/libGin"
 	"github.com/hmmftg/requestCore/response"
 	"github.com/hmmftg/requestCore/webFramework"
+	"github.com/valyala/fasthttp"
 )
 
 type ContextKey string
@@ -21,15 +23,17 @@ const (
 
 func InitContext(c context.Context) webFramework.WebFramework {
 	w := webFramework.WebFramework{}
-	switch c.Value(WebFrameworkKey) {
-	case Fiber:
-		w.Parser = libFiber.InitContext(c)
-	case Gin:
+	switch c.(type) {
+	case *gin.Context:
+		w.Ctx = context.WithValue(c, WebFrameworkKey, Gin)
 		w.Parser = libGin.InitContext(c)
+	case *fasthttp.RequestCtx:
+		w.Ctx = context.WithValue(c, WebFrameworkKey, Fiber)
+		w.Parser = libFiber.InitContext(c)
 	default:
 		log.Fatalf("error in InitContext: %s is unknown webFramework", c.Value(WebFrameworkKey).(string))
 	}
-	w.Ctx = context.WithValue(c, USER, w.Parser.GetHeaderValue("User-Id"))
+	w.Ctx = context.WithValue(w.Ctx, USER, w.Parser.GetHeaderValue("User-Id"))
 	return w
 }
 
