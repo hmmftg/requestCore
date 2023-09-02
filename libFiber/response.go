a76cfd5ec2b09a504bb7ea06a85aec3761a0b7f7
hmmftg <hmmftg@gmail.com>
2023-09-02 18:42:54 +0330

    fix: fiber error handler

--- libFiber/response.go
+++ libFiber/response.go
@@ -5,33 +5,40 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/hmmftg/requestCore/webFramework"
 )
 
-func ErrorHandler(path, title string, respondHandler func(int, int, string, any, bool, any)) fiber.ErrorHandler {
+type ContextInitiator interface {
+	InitContext(c *fiber.Ctx) webFramework.WebFramework
+	Respond(int, int, string, any, bool, webFramework.WebFramework)
+}
+
+func ErrorHandler(path, title string, handler ContextInitiator) fiber.ErrorHandler {
 	log.Println("ErrorHandler: ", path, title)
 	return func(c *fiber.Ctx, err error) error {
+		w := handler.InitContext(c)
 		log.Println(path, title, "ErrorHandler", err)
 		switch err := err.(type) {
 		case *fiber.Error:
 			switch err.Code {
 			case 404:
-				respondHandler(http.StatusNotFound, 1, "PAGE_NOT_FOUND", err, true, c)
+				handler.Respond(http.StatusNotFound, 1, "PAGE_NOT_FOUND", err, true, w)
 				return nil
 			}
 			log.Println("Fiber Error", err.Code, err.Message)
-			respondHandler(http.StatusInternalServerError, 1, "INTERNAL_ERROR", err, true, c)
+			handler.Respond(http.StatusInternalServerError, 1, "INTERNAL_ERROR", err, true, w)
 		default:
 			if c.Locals("LastError") != nil {
 				log.Println("LocalError", err)
-				respondHandler(http.StatusInternalServerError, 1, c.Locals("LastError").(string), nil, true, c)
+				handler.Respond(http.StatusInternalServerError, 1, c.Locals("LastError").(string), nil, true, w)
 				return nil
 			}
 			log.Println("Unknown", err)
-			respondHandler(http.StatusInternalServerError, 1, "INTERNAL_ERROR", nil, true, c)
+			handler.Respond(http.StatusInternalServerError, 1, "INTERNAL_ERROR", nil, true, w)
 			return nil
 		}
 		log.Println("Unknown", err)
-		respondHandler(http.StatusInternalServerError, 1, "INTERNAL_ERROR", nil, true, c)
+		handler.Respond(http.StatusInternalServerError, 1, "INTERNAL_ERROR", nil, true, w)
 		return nil
 	}
 }
