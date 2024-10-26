083e47066bc36ef74864c067e39b8315f89a523f
hmmftg <h.malekmohammadi@pooya.ir>
2024-10-26 16:41:43 +0330

    update sm

--- libCrypto/sm.go
+++ libCrypto/sm.go
@@ -6,9 +6,14 @@ type Sm interface {
 	Cvv(pan, exp, cvvType string) (string, response.ErrorState)
 	Pvv(pan, pinBlock string) (string, response.ErrorState)
 	Offset(pan, pinBlock string) (string, response.ErrorState)
+	Mac(data string) (string, response.ErrorState)
+	Translate(pan, pinBlock, tpk2nd string) (string, response.ErrorState)
+	Crypt(data, mode string) (string, response.ErrorState)
 }
 
 const (
-	Cvv1 = "Cvv1"
-	Cvv2 = "Cvv2"
+	Cvv1    = "Cvv1"
+	Cvv2    = "Cvv2"
+	Encrypt = "E"
+	Decrypt = "D"
 )
