c0b634d6d58fc769322dcb3f0c6026a1f61bb873
hmmftg <h.malekmohammadi@pooya.ir>
2024-10-27 15:09:44 +0330

    chore: add get key and set ket methods to sm

--- libCrypto/sm.go
+++ libCrypto/sm.go
@@ -3,6 +3,8 @@ package libCrypto
 import "github.com/hmmftg/requestCore/response"
 
 type Sm interface {
+	SetKey(id, value string)
+	GetKey(id string) string
 	Cvv(pan, exp, cvvType string) (string, response.ErrorState)
 	Pvv(pan, pinBlock string) (string, response.ErrorState)
 	Offset(pan, pinBlock string) (string, response.ErrorState)
