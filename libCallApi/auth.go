275c8f80152d75571b6d70574c3c27728485e2f2
hmmftg <hmmftg@gmail.com>
2025-02-08 17:19:03 +0330

    chore: add slog attr to remote-api auth

--- libCallApi/auth.go
+++ libCallApi/auth.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hmmftg/requestCore/libError"
 	"github.com/hmmftg/requestCore/status"
+	"github.com/hmmftg/requestCore/webFramework"
 )
 
 type Auth struct {
@@ -43,7 +44,7 @@ func InitTokenCache() (*TokenCache, *sync.Mutex) {
 }
 
 type AuthSystem interface {
-	Login() (*TokenCache, libError.Error)
+	Login(w webFramework.WebFramework) (*TokenCache, libError.Error)
 }
 
 func (api RemoteApi) GetBasicAuthHeader() string {
@@ -69,14 +70,14 @@ func (api RemoteApi) GetAuthHeader() (string, error) {
 	return fmt.Sprintf("%s %s", api.TokenCache.AccessToken.Type, api.TokenCache.AccessToken.Token), nil
 }
 
-func (api *RemoteApi) handleToken() libError.Error {
+func (api *RemoteApi) handleToken(w webFramework.WebFramework) libError.Error {
 	api.TokenCacheLock.Lock()
 	defer api.TokenCacheLock.Unlock()
 
 	api.TokenCache.AccessToken = nil
 	api.TokenCache.RefreshToken = nil
 
-	tokens, err := api.Auth.Login()
+	tokens, err := api.Auth.Login(w)
 	if err != nil {
 		return err
 	}
@@ -85,18 +86,18 @@ func (api *RemoteApi) handleToken() libError.Error {
 	return nil
 }
 
-func (api *RemoteApi) Authenticate() libError.Error {
+func (api *RemoteApi) Authenticate(w webFramework.WebFramework) libError.Error {
 	if api.TokenCacheLock == nil {
 		return libError.New(status.InternalServerError, "TOKEN_CACHE_NOT_INITIALIZED", "token cache lock of api %s is null", api.Name)
 	}
 	if api.TokenCache.AccessToken == nil {
-		err := api.handleToken()
+		err := api.handleToken(w)
 		if err != nil {
 			return err
 		}
 	}
 	if api.TokenCache.Expired() {
-		err := api.handleToken()
+		err := api.handleToken(w)
 		if err != nil {
 			return err
 		}
